0faca6c552c302688bb04c617a2acf738a71ae2b
Quentin Cavillier <quentin.cavillier@epfl.ch>
2018-11-16 14:22:47 +0100

    TO CHANGE! Firebase credentials

---
+++ public/scripts/fb.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "yourApiKey",
+    authDomain: "yourPlatform.firebaseapp.com",
+    databaseURL: "https://yourPlatform.firebaseio.com",
+    projectId: "yourPlatform",
+    storageBucket: "yourPlatform.appspot.com",
+    messagingSenderId: "123456789"
+  };
+  firebase.initializeApp(config);
